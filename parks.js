d6a2d6194a44d2ff5f237d03261f398e571a6a3f
Andrew <nba.andy@gmail.com>
2017-05-09 08:23:03 +1200

    Added in Parks test code.

--- parks.js
+++ parks.js
@@ -1,1 +1,41 @@
 console.log('hi');
+
+var parks = {
+	"Maclean": [{
+		"Designation": [
+			"Child Friendly",
+			"Dog Friendly",
+			"Water Park",
+			"Rugby Ground",
+			"Soccer Field"
+		],
+		"Suburb": "Paraparaumu"
+	}],
+	"Waikanae": [{
+		"Designation": [
+			"Child Friendly",
+			"Dog Friendly",
+			"Water Park",
+			"Rugby Ground",
+			"Soccer Field"
+		],
+		"Suburb": "Waikanae"
+	}]
+}
+
+//Return all designations for parks for a selected suburb
+function findSuburbDesignations(suburb) {
+	
+	var designations = [];
+	$.each(parks, function(park, attrs) {
+		console.log(park);
+		$.each(attrs, function(i, item) {
+			if(item['Suburb'] == suburb){
+				designations = designations.concat(item['Designation']);
+			}
+		});
+	});
+	
+	console.log('Designations');
+	console.log(designations);
+}
