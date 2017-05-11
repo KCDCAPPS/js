1246f323ce4e03cbfccbde1bc04536226471475f
Andrew <nba.andy@gmail.com>
2017-05-12 08:12:44 +1200

    no message

--- parks.js
+++ parks.js
@@ -82,9 +82,9 @@ window.onload = function() {
 			}]
 		}
 		
-		function addNewPark(park){
+		function addNewPark(park, parkAttributes){
 			var parkList = [
-				'<a href="'+ park['Webpage'] + '">',
+				'<a href="'+ parkAttributes.Webpage + '">',
 				'<div class="row">',
 					"<div class='well col-md-12' style='background-color: #ffffff; margin-top: 10px; cursor: pointer;'>",
 						"<div class='col-md-4'>",
@@ -124,7 +124,7 @@ window.onload = function() {
 									designations.push(parkDesignation);
 								}
 							});	
-							addNewPark(park)						
+							addNewPark(park, attrs)						
 						}
 					});
 				});
