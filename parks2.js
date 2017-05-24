2943780722090221a19a2b265c5c81645f391a86
Andrew <nba.andy@gmail.com>
2017-05-24 13:06:22 +1200

    Removed fancybox test JS

--- parks2.js
+++ parks2.js
@@ -448,24 +448,4 @@ window.onload = function() {
 			$('.well').parent().parent().css({ 'color': 'inherit' });
 		}
 		
-		/* This is basic - uses default settings */
-	
-		$("a#single_image").fancybox();
-		
-		/* Using custom settings */
-		
-		$("a#inline").fancybox({
-			'hideOnContentClick': true
-		});
-
-		/* Apply fancybox to multiple items */
-		
-		$("a.group").fancybox({
-			'transitionIn'	:	'elastic',
-			'transitionOut'	:	'elastic',
-			'speedIn'		:	600, 
-			'speedOut'		:	200, 
-			'overlayShow'	:	false
-		});
-
 }
