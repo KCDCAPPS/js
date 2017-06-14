84a5981bd4d6bd34ab97390264b083e031696efa
kat <katherine.scott.nz@gmail.com>
2017-06-15 09:58:03 +1200

    no message

--- parks2.js
+++ parks2.js
@@ -249,7 +249,7 @@ window.onload = function() {
 				"Location": "Whyte Street, Paraparaumu Beach",
 				"Webpage": "http://stuff.co.nz",
 				"Blurb": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
-				"Image": "/globalassets/our-district/our-community/parks-and-recreation/kena.png"
+				"Image": "/globalassets/our-district/our-community/parks-and-recreation/kenacricket.png"
 			}],
 			"Kotuku Reserve": [{
 				"Designation": [
