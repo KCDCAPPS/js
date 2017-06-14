0d4813087a11704af296fab928f499649546f706
kat <katherine.scott.nz@gmail.com>
2017-06-15 09:25:18 +1200

    Maclean Park image added correctly

--- parks2.js
+++ parks2.js
@@ -312,7 +312,7 @@ window.onload = function() {
 				"Location": "Marine Parade, Paraparaumu Beach",
 				"Webpage": "http://stuff.co.nz",
 				"Blurb": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
-				"Image": "/globalassets/our-district/our-community/parks-and-recreation/macleanpark.png"
+				"Image": "/globalassets/our-district/our-community/parks-and-recreation/Macleanpark.png"
 			}],
 			"Main Street Playground": [{
 				"Designation": [
