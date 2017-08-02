4d161ff6d48a168a7a5df750b69a76aca7058424
Andrew <nba.andy@gmail.com>
2017-08-03 09:47:04 +1200

    no message

--- parks2.js
+++ parks2.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 		var suburb = '';
 		var visibleParks = 0;
-		var domain = "http://uat.kapiticoast.govt.nz.testwin.gdmedia.tv/";
+		var domain = "http://www.uat.kapiticoast.govt.nz.testwin.gdmedia.tv/";
 
 		$('#suburb').select2({
 			placeholder: "Select a Suburb",
