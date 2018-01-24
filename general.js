357ef55e0b4b6fe5d8a71266793208bb756ac903
Andrew <nba.andy@gmail.com>
2018-01-25 08:39:52 +1300

    add console.log for testing purposes

--- general.js
+++ general.js
@@ -3,5 +3,6 @@ var domain = "http://uat.kapiticoast.govt.nz.testwin.gdmedia.tv/";
 //var domain = "http://www.kapiticoast.govt.nz/";
 
 $('#cemeteries').select(function() {
+	console.log('Hi');
 	window.open(domain + $(this).val());
 });
