ebaae66d78712d25b50073c849ece181a2411d49
Andrew <nba.andy@gmail.com>
2018-01-25 08:55:02 +1300

    removed console.log.

--- general.js
+++ general.js
@@ -3,6 +3,5 @@ var domain = "http://uat.kapiticoast.govt.nz.testwin.gdmedia.tv/";
 //var domain = "http://www.kapiticoast.govt.nz/";
 
 $('#cemeteries').change(function() {
-	console.log('Hi');
 	window.open(domain + $(this).val());
 });
